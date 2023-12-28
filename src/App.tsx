f0bf5b46fcc4081a1d0071669f83d5abbefc4e67
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-29 00:33:33 +0600

    Added Routes for edit dish and new dish

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
-import Admin from './components/Admin';
+import Admin from './components/Admin/Admin';
+import Dishes from './components/Dishes/Dishes';
+import NewDish from './components/NewDish/NewDish';
+import EditDish from './components/EditDish/EditDish';
 
 function App() {
 
   return (
     <Routes>
       <Route path="/admin" element={<Admin />} />
+      <Route path="/admin/dishes" element={(<Dishes/>)} ></Route>
+      <Route path="/admin/new-dish" element={(<NewDish/>)} ></Route>
+      <Route path="/admin/edit-dish/:id" element={<EditDish/>}></Route>
     </Routes>
   );
 }

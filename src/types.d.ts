62e59cb8b0ad64df3faed5403d7771a3f6667785
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-29 00:32:25 +0600

    Changed types for thunks

---
+++ src/types.d.ts
@@ -0,0 +1,18 @@
+export interface Dish {
+  id: string;
+  title: string;
+  price: number;
+  image: string;
+}
+
+export type ApiDish = Omit<Dish, 'id'>;
+
+export interface DishesList {
+  [id:string]: ApiDish;
+}
+
+export interface DishMutation {
+  title: string;
+  price: string;
+  image: string;
+}

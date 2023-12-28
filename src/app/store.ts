136af4a63a671d3812a1934ca7a42164aa5ae701
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-29 00:32:12 +0600

    Added dishReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
+import {dishesReducer} from '../store/dishes/dishesSlice';
 export const store = configureStore({
   reducer: {
-
+    dishes: dishesReducer,
   }
 });
 

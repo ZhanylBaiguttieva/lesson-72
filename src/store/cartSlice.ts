ce1b4f50663d53ba26d8314076d6f1cb895db7aa
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-29 01:49:22 +0600

    Added cartSlice for cart

---
+++ src/store/cartSlice.ts
@@ -0,0 +1,51 @@
+import {CartMeat, Dish} from '../types';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {RootState} from '../app/store';
+
+interface CartState {
+  cartMeats: CartMeat[];
+}
+
+const initialState: CartState = {
+  cartMeats:[],
+};
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addMeat: (state, {payload: meat}: PayloadAction<Dish>) => {
+      const index = state.cartMeats.findIndex(cartMeat => cartMeat.meat.id === meat.id);
+
+      if (index !== -1) {
+        state.cartMeats[index].amount++;
+      } else {
+        state.cartMeats.push({
+          amount: 1,
+          meat,
+        });
+      }
+    },
+    updateCart: (state, {payload: meats}: PayloadAction<Dish[]>) => {
+      const newCartMeats: CartMeat[] = [];
+
+      state.cartMeats.forEach(cartMeat => {
+        const existingMeat = meats.find(meat => meat.id === cartMeat.meat.id);
+        if(!existingMeat) {
+          return;
+        }
+
+        newCartMeats.push({
+          ...cartMeat,
+          meat: existingMeat,
+        });
+      });
+      state.cartMeats = newCartMeats;
+    },
+    clearCart: (state) => {
+      state.cartMeats = [];
+    },
+  }
+});
+export const cartReducer = cartSlice.reducer;
+export const {addMeat, clearCart, updateCart} = cartSlice.actions;
+export const selectCartMeats = (state:RootState) => state.cart.cartMeats;
